8d82a6f3a31a94beac52cfe3a9e5fa335138dbd0
Miriam Duran <mduran@upc.edu>
2025-10-07 22:17:03 +0200

    Puesto Link con Header y páginas de Home y jugadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home';
+import { JugadoresComponent } from './components/jugadores/jugadores';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'jugadores', component: JugadoresComponent }
+];
---
+++ src/app/components/jugadores/jugadores.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugadores',
+  templateUrl: './jugadores.html',
+  styleUrls: ['./jugadores.css']
+})
+export class JugadoresComponent { }
