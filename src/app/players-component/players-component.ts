675266b10f8d6a8ef2ab0c17f507230f3fc43ac6
bernatbm <bernyybm@gmail.com>
2025-10-12 12:21:56 +0200

    Add Detail,Media and Players Components

---
+++ src/app/players-component/players-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-players-component',
+  imports: [],
+  templateUrl: './players-component.html',
+  styleUrl: './players-component.css'
+})
+export class PlayersComponent {
+
+}
