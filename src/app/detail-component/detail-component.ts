8d65f4982e22f30c6ffe41bf7a18e85cf9a940aa
GerardCamell <gerard_bcn_cabo@hotmail.es>
2025-10-23 14:09:20 +0200

    media fet

--- src/app/detail-component/detail-component.ts
+++ src/app/detail-component/detail-component.ts
@@ -1,27 +1,28 @@
-import { Component,Input } from '@angular/core';
-import {CommonModule} from '@angular/common';
-export interface Player{
-  name:string;
-  lastName:string;
-  position:string;
-  age:number;
-  height:string;
-  weight:number;
-  alias:number;
-  teams:string;
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MediaComponent } from '../media-component/media-component';
+
+export interface Player {
+  name: string;
+  lastName: string;
+  position: string;
+  age: number;
+  height: string;
+  weight: number;
+  alias: number;
+  teams: string;
   headshot: string;
-  initials:string;
+  initials: string;
+  video: string;
 }
 
 @Component({
   selector: 'app-detail-component',
-  standalone:true,
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, MediaComponent], 
   templateUrl: './detail-component.html',
-  styleUrl: './detail-component.css'
+  styleUrls: ['./detail-component.css'] 
 })
 export class DetailComponent {
- @Input() player: Player | null = null;
- 
-
+  @Input() player: Player | null = null;
 }
--- src/app/media-component/media-component.ts
+++ src/app/media-component/media-component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-media-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './media-component.html',
-  styleUrl: './media-component.css'
+  styleUrls: ['./media-component.css']
 })
 export class MediaComponent {
+  @Input() videoUrl: string = '';
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
+  play() {
+    this.videoPlayer.nativeElement.play();
+  }
+
+  pause() {
+    this.videoPlayer.nativeElement.pause();
+  }
+
+  stop() {
+    const video = this.videoPlayer.nativeElement;
+    video.pause();
+    video.currentTime = 0;
+  }
 }
