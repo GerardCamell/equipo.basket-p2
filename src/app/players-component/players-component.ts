80faf25c852e1ecd710c350c365ace90ff4099ac
GerardCamell <gerard_bcn_cabo@hotmail.es>
2025-11-06 11:20:01 +0100

    jugadores mediante firebase 5/10

--- src/app/players-component/players-component.ts
+++ src/app/players-component/players-component.ts
@@ -1,88 +1,38 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { Player, DetailComponent } from '../detail-component/detail-component';
 import { PlayerFilterPipe } from '../pipes/player-filter-pipe';
-import { FormsModule } from '@angular/forms';
-//a
+
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-players-component',
-  standalone:true,
-  imports: [CommonModule, DetailComponent, FormsModule, PlayerFilterPipe],
+  standalone: true,
+  imports: [CommonModule, FormsModule, DetailComponent, PlayerFilterPipe],
   templateUrl: './players-component.html',
   styleUrl: './players-component.css'
 })
 export class PlayersComponent {
 
-  searchText: string = '';    
-  filterBy: string = 'name';
   
-  teams: Player[] = [{
-    name:'Dalen',
-    lastName:'Terry',
-    position: 'SG',
-    age:23,
-    height:'6-7',
-    weight:195,
-    alias: 7,
-    teams: "Chicago Bulls",
-    headshot: "/HEADSHOTS/Terry_Dalen.png",
-    initials:"CHI",
-    video: "/videos/dalenTerry.mp4"
-  },
-  {
-    name:'Julian',
-    lastName:'Phillips',
-    position: 'SF',
-    age:21,
-    height:'6-8',
-    weight:198,
-    alias:15,
-    teams: "Chicago Bulls",
-    headshot: "/HEADSHOTS/Phillips_Julian.png",
-    initials:"CHI",
-    video:"/videos/julianPhillips.mp4"
-  },
-  {
-    name:'Noa',
-    lastName:'Essengue',
-    position: 'PF',
-    age:18,
-    height:'6-9',
-    weight:194,
-    alias:24,
-    teams: "Chicago Bulls",
-    headshot: "/HEADSHOTS/Essengue_Noa.png",
-    initials:"CHI",
-    video:"/videos/noaEssengue.mp4"
-  },
-  {
-    name:'Jalen',
-    lastName:'Smith',
-    position: 'F-C',
-    age:25,
-    height:'6-9',
-    weight:215,
-    alias:25,
-    teams: "Chicago Bulls",
-    headshot: "/HEADSHOTS/Smith_Jalen.png",
-    initials:"CHI",
-    video:"/videos/jalenSmith.mp4"
-  },
-  {
-    name:'Tre',
-    lastName:'Jones',
-    position: 'PG',
-    age:18,
-    height:'6-1',
-    weight:185,
-    alias:30,
-    teams: "Chigago Bulls",
-    headshot: "/HEADSHOTS/Jones_Tre.png",
-    initials:"CHI",
-    video:"/videos/treJones.mp4"
-  },
-];
-selectedPlayer: any;
+  searchText: string = '';
+  filterBy: string = 'name';
 
+  
+  players$: Observable<Player[]>;
+
+  
+  selectedPlayer: Player | null = null;
+
+  constructor(private firestore: Firestore) {
+    const ref = collection(this.firestore, 'players');
+    this.players$ = collectionData(ref, { idField: 'id' }) as Observable<Player[]>;
+  }
+
+  selectPlayer(p: Player) {
+    this.selectedPlayer = p;
+  }
 }
