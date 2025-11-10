185b8b42e30597ea225d94ae6f6838a9f02e53da
bernatbm <75563357+bernatbm@users.noreply.github.com>
2025-11-10 12:25:03 +0100

    Añadido, empezando, metodo CRUD.Creado un construtor de Player y formulario(esta a medias,empezado) para poder añadir, modificar y eliminar a los jugadores.

---
+++ src/app/form-player/form-player.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Player } from '../detail-component/player.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-player',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './form-player.html',
+  styleUrl: './form-player.css',
+})
+export class FormPlayer {
+  newPlayer: Player = new Player();
+
+  @Output() onAdd = new EventEmitter<Player>();
+
+  createPlayer() {
+    this.onAdd.emit(this.newPlayer);
+    this.newPlayer = new Player();
+  };
+  
+  
+
+
+} 
+
