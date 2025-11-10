dd0dab588199199613cb33ed9b2da6b10eea6952
bernatbm <75563357+bernatbm@users.noreply.github.com>
2025-11-10 12:25:03 +0100

    Añadido, empezando, metodo CRUD.Creado un construtor de Player y formulario(esta a medias,empezado) para poder añadir, modificar y eliminar a los jugadores.

---
+++ src/app/player-service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
+import { Player } from './detail-component/player.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlayerService {
+  private playersCollection;
+
+  constructor(private firestore: Firestore) {
+    this.playersCollection = collection(this.firestore, 'players');
+  }
+
+  //CRUD metodos service.
+
+  createPlayer(player:Player){
+    //Create jugador
+    return addDoc(this.playersCollection, player);
+  }
+
+  readPlayers():Observable<Player[]> {
+    //Read jugadores
+    return collectionData(this.playersCollection, { idField: 'id' }) as Observable<Player[]>;
+  }
+  
+  updatePlayer(player:Player){
+    //Update de los jugadores
+    const playerDoc =  doc(this.firestore, `players/${player.id}`);
+    // Excluimos el id del objeto para no actualizarlo en FireStore
+    const { id, ...playerData } = player;
+    return updateDoc(playerDoc, playerData);
+  }  
+
+  deletePlayer(playerId:number){
+    //Delete del jugador
+    const playerDoc =  doc(this.firestore, `players/${playerId}`);
+    return deleteDoc(playerDoc);
+  }
+}
