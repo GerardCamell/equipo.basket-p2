98f72c8f6f17ea24516f37dd2930e0d72f59c2b6
bernatbm <75563357+bernatbm@users.noreply.github.com>
2025-11-11 14:42:23 +0100

    Se puede crear, eliminar y editar jugador(falta por hacer, poder crear foto, video y initial)

---
+++ src/app/selected-player/selected-player.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-selected-player',
+  imports: [],
+  templateUrl: './selected-player.html',
+  styleUrl: './selected-player.css',
+})
+export class SelectedPlayer {
+
+  //Proximamente pondremos aquí para obtener el jugador seleccionado, ahora no se como se hace :'(
+}
