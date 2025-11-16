fe4f9d711d035a5f1e1df09e9dbf865030b99413
bernatbm <75563357+bernatbm@users.noreply.github.com>
2025-11-16 21:00:08 +0100

    Finiquitado, Se ha creado un node.js para hacer de backend a la hora de subir los headshots y videos.Se ha creado en el formulario un botón para volver para atras en caso de no querer añadir o actualizar un jugador.Se ha arreglado un problema que había a la hora de cambiar el video del jugador seleccionado(no actualizaba al jugador seleccionado, se quedaba el anterior) Se han añadido (Update) de los jugadores añadidos antes de que funcionará la opcion de añadir fotos y videos, hay un video duplicado de Ayo_Dousunmu.mp4 que es un duplicado de otro video.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     apiKey: "AIzaSyBUUF5jhHNx2TON-UsgyZ0LyssrWHC2gaw",
     authDomain: "equipo-basket-p2.firebaseapp.com",
     projectId: "equipo-basket-p2",
-    storageBucket: "equipo-basket-p2.firebasestorage.app",
+    storageBucket: "equipo-basket-p2.appspot.com",
     messagingSenderId: "474074122785",
     appId: "1:474074122785:web:a3440b09aa77b492a2f160",
     measurementId: "G-0T52MVSGHZ"
