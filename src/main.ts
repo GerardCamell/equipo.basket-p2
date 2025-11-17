bfa52fd3c20e51207eaf4d0d34ee0d3b4b3c16e3
Carmen <czunigam@uoc.edu>
2025-11-17 21:03:41 +0000

    firebase conexion

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "equipo-basket-p2", appId: "1:474074122785:web:a3440b09aa77b492a2f160", databaseURL: "https://equipo-basket-p2-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "equipo-basket-p2.firebasestorage.app", apiKey: "AIzaSyBUUF5jhHNx2TON-UsgyZ0LyssrWHC2gaw", authDomain: "equipo-basket-p2.firebaseapp.com", messagingSenderId: "474074122785", measurementId: "G-0T52MVSGHZ", projectNumber: "474074122785", version: "2" })), provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
